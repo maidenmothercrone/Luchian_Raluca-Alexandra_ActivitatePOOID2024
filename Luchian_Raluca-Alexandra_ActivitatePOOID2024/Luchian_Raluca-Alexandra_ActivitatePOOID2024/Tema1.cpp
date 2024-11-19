9657d62ed71dbdc14138bdc6cb9f6a1d53a11716
maidenmothercrone <ralucaalexandraluchian@gmail.com>
2024-11-19 14:20:51 +0200

    Codul final cu mici modificari pentru afisare si prevenire memory leaks

--- Luchian_Raluca-Alexandra_ActivitatePOOID2024/Luchian_Raluca-Alexandra_ActivitatePOOID2024/Tema1.cpp
+++ Luchian_Raluca-Alexandra_ActivitatePOOID2024/Luchian_Raluca-Alexandra_ActivitatePOOID2024/Tema1.cpp
@@ -21,20 +21,13 @@ public:
 	Autobuz(int capacitate, int nrPersoaneImbarcate, const char* producator) :idAutobuz(++nrAutobuze) {
 		this->capacitate = capacitate;
 		if (nrPersoaneImbarcate > capacitate) {
-			cout << "Prea multe persoane imbarcate pentru numarul de scaune disponibile. Ajustez numarul de pasageri in functie de acest factor.";
+			cout << "Prea multe persoane imbarcate pentru numarul de scaune disponibile. Ajustez numarul de pasageri in functie de acest factor." << endl;
 			this->nrPersoaneImbarcate = capacitate;
 		}
 		else this->nrPersoaneImbarcate = nrPersoaneImbarcate;
 		this->producator = new char[strlen(producator) + 1];
 		strcpy_s(this->producator, strlen(producator) + 1, producator);
 
-	};
-
-	//destructor
-	~Autobuz() {
-		if (this->producator != nullptr) {
-			delete[] this->producator;
-		}
 	};
 
 	//constructor copiere
@@ -45,6 +38,13 @@ public:
 		strcpy_s(this->producator, strlen(altul.producator) + 1, altul.producator);
 	};
 
+	//destructor
+	~Autobuz() {
+		if (this->producator != nullptr) {
+			delete[] this->producator;
+		}
+	};
+
 	//metoda get pentru producator
 	const char* getProducator() {
 		return this->producator;
@@ -52,12 +52,15 @@ public:
 
 	//metoda set pentru producator
 	void setProducator(const char* nouProducator) {
+		if (this->producator != nullptr) {
+			//eliberare memorie existenta
+			delete[] this->producator;  
+		}
 		this->producator = new char[strlen(nouProducator) + 1];
 		strcpy_s(this->producator, strlen(nouProducator) + 1, nouProducator);
 	}
 
 	//metoda get pentru pasageri
-
 	int getNrPersoaneImbarcate() {
 		return this->nrPersoaneImbarcate;
 	}
@@ -121,9 +124,9 @@ public:
 		cout << "Capacitate: " << this->capacitate << endl;
 		cout << "Numar persoane imbarcate: " << this->nrPersoaneImbarcate << endl;
 		if (this->producator != nullptr) {
-			cout << "Producator: " << this->producator << endl;
+			cout << "Producator: " << this->producator << endl<<endl;
 		}
-		else cout << "Nu are producator" << endl;
+		else cout << "Nu are producator" << endl<<endl;
 	}
 };
 
@@ -132,29 +135,32 @@ int Autobuz::nrAutobuze = 0;
 int main() {
 	//testare constructor fara parametrii
 	Autobuz autobuz0;
+	cout << "Test constructor fara parametrii: " << endl;
 	autobuz0.afisareAutobuz();
 
 	//testare constructor cu parametrii
 	Autobuz autobuz1(20, 21, "Volswagen");
+	cout << "Test constructor cu parametrii: " << endl;
 	autobuz1.afisareAutobuz();
 
 	//testare constructor copiere
 	Autobuz autobuzCopie = autobuz1;
+	cout << "Test constructor de copiere: " << endl;
 	autobuzCopie.afisareAutobuz();
 
 	//testare getteri si setteri pt nr pasageri (persoane imbarcate) si producator
+	cout << "Test getteri si setter pentru numar persoane imbarcate si producator: " << endl;
 	cout << "Producatorul initial pentru autobuzul 2 este: " << autobuz1.getProducator() << endl;
 	autobuz1.setProducator("Toyota");
 	cout << "Noul producator pentru autobuzul 2 este: " << autobuz1.getProducator() << endl;
-
 	cout << "Numarul initial de persoane imbarcate in autobuzul 1 este: " << autobuz0.getNrPersoaneImbarcate() << endl;
 	autobuz0.setNrPersoaneImbarcate(1);
-	cout << "Noul numar de persoane imbarcate in autobuzul 1 este: " << autobuz0.getNrPersoaneImbarcate() << endl;
+	cout << "Noul numar de persoane imbarcate in autobuzul 1 este: " << autobuz0.getNrPersoaneImbarcate() << endl<<endl;
 
 	//testare supraincarcare operator de atribuire
+	cout << "Testare supraincarcare operator=: " << endl;
 	Autobuz autobuz2(40, 30, "Mercedes");
 	Autobuz autobuz3(36, 20, "Volvo");
-
 	cout << "Autobuz 2:" << endl;
 	autobuz2.afisareAutobuz();
 	cout << endl;
@@ -174,19 +180,23 @@ int main() {
 	autobuz2.afisareAutobuz();
 
 	//testare supraincarcare operator <<
+	cout << "Test supraincarcare operator<<: " << endl;
 	cout << "Autobuz initial: " << autobuz0 << endl;
 	cout << "Autobuz 1: " << autobuz1 << endl;
 	cout << "Autobuz 2: " << autobuz2 << endl;
-	cout << "Autobuz 3: " << autobuz3 << endl;
+	cout << "Autobuz 3: " << autobuz3 << endl<<endl;
 
 	//testare metoda de returnare locuri libere
-	cout << "Numar locuri libere in autobuz 2: " << autobuz2.getNumarLocuriLibere() << endl;
+	cout << "Test metoda de returnare locuri libere: " << endl;
+	cout << "Numar locuri libere in autobuz 2: " << autobuz2.getNumarLocuriLibere() << endl<<endl;
 
 	//testare supraincarcare operator de cast la int
+	cout << "Test supraincarcare operator de cast la int: " << endl;
 	int persoaneImbarcate = autobuz3;
-	cout << "Numarul de persoane imbarcate in autobuzul 3 este: " << persoaneImbarcate << endl;
+	cout << "Numarul de persoane imbarcate in autobuzul 3 este: " << persoaneImbarcate << endl<<endl;
 
 	//testare supraincarcare operator >
+	cout << "Test supraincarcare operator>: " << endl;
 	(autobuz2 > autobuz3) ? (cout << "Autobuzul 2 are o capacitate mai mare decat autobuzul 3.") : (cout << "Autobuzul 2 are o capacitate mai mica sau egala cu cea a autobuzului 3");
 
 }
